f7289ffd7205dd5108af197c1990a42ab54bf265
GonzaSai <49086284@ivt.edu.ar>
2025-04-18 18:54:16 -0300

    Proyecto1v2.0

---
+++ script.js
@@ -0,0 +1,10 @@
+let boton = document.querySelector('#modoOscuro')
+let body = document.querySelector('body')
+let boton2y3 = document.querySelector('.btn-info')
+boton.onclick = function(){
+    body.classList.toggle('dark')
+}
+
+boton2y3.onclick = function(){
+    window.location.href="https://youtu.be/dQw4w9WgXcQ?si=J1VzPQ75PNfGMdnv"
+}
